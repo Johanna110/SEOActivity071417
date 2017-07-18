4660d39f224609f4c215e4a19be06ce8a6ac8921
Johanna110 <ulangkayaj42@gmail.com>
2017-07-18 18:59:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,147 @@
+<html>
+   <head>
+      <title>Online Slambook</title>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">      
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>           
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script> 
+   </head>
+
+   <body class="container">  
+
+	<h1> 
+		<div class="card-panel #ef9a9a red lighten-3" align="center">Tell Me About Yourself</div>					
+	</h1>
+
+   <div class="container">
+      	<div class="row">
+        <form class="col s12" >
+        <div class="row">
+		<br><br><br>
+		<h5 align="center"> </h5>          
+	    <div class="row">
+		<form class="col s6">
+	    <div class="row">			     			       
+    </div>
+
+				  	<div class="input-field col s12">	       	
+			   			 <input id="icon_prefix" type="text" class="validate">
+			    		 <label for="icon_prefix">Your Name</label>
+			 		</div>	
+
+			      	<div class="input-field col s12">			    
+			        	 <input id="icon_prefix" type="text" class="validate">
+			         	 <label for="icon_prefix"> Nickname</label>
+			        </div>
+
+			        <div class="input-field col s12">			        
+			         	 <input id="icon_prefix" type="number" class="validate">
+			          	 <label for="icon_prefix">Phone</label>
+			       	</div>	
+
+			        <div class="input-field col s12">
+			             Birthday<br><br>			         	
+			         	 <input type="date" class="timepicker">
+			        </div>
+			    
+                    <div class="input-field col s12">
+                    	 <p>Gender</p>
+                    	 <p>
+                    	 <input id="male" type="radio" name="gender" value="male" checked>
+                    	 <label for="male">Male</label>
+                    	 </p>
+                    	 <p>
+                    	 <input id="female" type="radio" name="gender" value="female" checked>
+                     	 <label for="female">Female</label>
+                    	 </p>                 
+                     </div>
+
+                     <div class="input-field col s12">			       
+			          	 <input id="icon_prefix" type="email" class="validate">
+			         	 <label for="icon_prefix">Email Address:</label>
+			        </div>	
+
+			        <div class="input-field col s12">			    
+			          	 <input id="icon_prefix" type="password" class="validate">
+			         	 <label for="icon_prefix">Password:</label>
+			        </div>
+
+			         <div class="input-field col s12">		        
+			         	 <input id="icon_prefix" type="text" class="validate">
+			          	 <label for="icon_prefix"> Hobbies:</label>
+			        </div>
+
+			        <div class="input-field col s12">
+			            <input id="icon_prefix" type="text" class="validate">			         			       
+			          	<label for="icon_prefix"> Who is your crush?</label>
+			        </div>
+
+			        <div class="input-field col s12">			      
+			         	 <input id="icon_prefix" type="text" class="validate">
+			         	 <label for="icon_prefix"> Who is your first love? </label>
+			        </div>
+
+			         <div class="input-field col s12">			      
+			         	 <input id="icon_prefix" type="text" class="validate">
+			         	 <label for="icon_prefix"> Who is your first kiss? </label>
+			        </div>
+
+			         <div class="input-field col s12">			      
+			         	 <input id="icon_prefix" type="text" class="validate">
+			         	 <label for="icon_prefix"> Your favorite past time? </label>
+			        </div>
+
+			        <div class="input-field col s12">			      
+			         	 <input id="icon_prefix" type="text" class="validate">
+			         	 <label for="icon_prefix"> Name of your bestfriend: </label>
+			        </div>
+
+			        <div class="input-field col s12">			      
+			         	 <input id="icon_prefix" type="text" class="validate">
+			         	 <label for="icon_prefix"> Favorite teacher: </label>
+			        </div>
+
+			        <div class="input-field col s12">			      
+			         	 <input id="icon_prefix" type="text" class="validate">
+			         	 <label for="icon_prefix"> Favorite subject: </label>
+			        </div>
+			   
+			        <div class="input-field col s12">		      
+			          	<input id="icon_prefix" type="text" class="validate">
+			          	<label for="icon_prefix">Favorite food:</label>
+			        </div>
+
+			        <div class="input-field col s12">			      
+			          	<input id="icon_prefix" type="email" class="validate">
+			          	<label for="icon_prefix">Favorite color:</label>
+			        </div>	
+
+			        <div class="input-field col s12">		     
+			         	 <input id="icon_prefix" type="password" class="validate">
+			         	 <label for="icon_prefix">Favorite movie:</label>
+			        </div>	
+
+            		<div class="row">           
+            			</div>			
+           				<div class="row">
+			        	<div class="input-field col s12">		   
+			         	<input id="icon_prefix" type="text" class="validate">
+			         	<label for="icon_prefix">Message for your crush: </label>
+           				</div>
+
+        
+           			 <div align="center">
+                		 <a href="/success" class="btn waves-effect waves-light" type="submit" name="action" role="button">Submit</a>
+     				 </div>
+
+            		 <h2> 
+            			<div class="card-panel #ef9a9a red lighten-3" align="center">Your secrets are safe with me!</div>					
+					 </h2>					
+				 
+
+         </form>       
+       </div>
+     </div>
+   </body>   
+</html>
